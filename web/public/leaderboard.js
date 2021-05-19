7ecddc23d43a6f01b90a310d56efc7e0915aa4bf
Garrett Wilhelm <26906320+gbwilhelm@users.noreply.github.com>
2021-05-19 16:46:18 -0400

    rework client-server

--- web/public/leaderboard.js
+++ web/public/leaderboard.js
@@ -1,6 +1,3 @@
-//require() does not work here, need to figure out import syntax
-//import { DescribeTableCommand } from "../@aws-sdk/client-dynamodb"
-
 var app = new Vue({
   el: '#leaderboard',
   data: {
@@ -16,25 +13,4 @@ var app = new Vue({
   methods: {
 
   },
-  /*beforeMount(){
-    console.log("creating ddb client...")
-    this.ddbClient = new DynamoDBClient({region:"us-east-1"})
-  },
-  mounted(){
-    // Set the parameters
-    const params = { TableName: "yacht-scores" }; //TABLE_NAME
-
-    console.log("request sent...")
-
-    const run = async () => {
-      try {
-        const data = await this.ddbClient.send(new DescribeTableCommand(params));
-        window.alert("Success\n"+data.Table.KeySchema);
-        return data;
-      } catch (err) {
-        window.alert("Error\n"+err);
-      }
-    };
-    run();
-  }*/
 })
--- web/server.js
+++ web/server.js
@@ -6,3 +6,29 @@ app.use(express.static("public"))
 
 app.listen(8080) //run server, access via ip:port
 console.log("server listening on port 8080")
+
+const initClient = function(){
+  console.log("creating ddb client...")
+  this.ddbClient = new DynamoDBClient({region:"us-east-1"})
+}
+
+const describeTable = function(){
+  const params = {TableName:"yacht-scores"};
+
+  console.log("request sent...")
+
+  const run = async () => {
+    try {
+      const data = await this.ddbClient.send(new DescribeTableCommand(params))
+      console.log("Success",data.Table.KeySchema)
+      return data
+    } catch (err) {
+      console.log("Error",err)
+    }
+  }; run()
+}
+
+//need to use express routing to relay info between client and server
+//server will interface with Database, then pipe results back to client
+
+//initClient(); describeTable();
