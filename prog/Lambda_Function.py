ae90b8ea61381e2d5496a9c78445d0eef6b5f0fc
Garrett Wilhelm <26906320+gbwilhelm@users.noreply.github.com>
2021-05-22 16:11:34 -0400

    Updated Lambda Function

--- prog/Lambda_Function.py
+++ prog/Lambda_Function.py
@@ -3,19 +3,19 @@ import os
 import boto3
 
 def lambda_handler(event, context):
+    print(event)
     message = ''
     for r in event['Records']:
+        #ignore non-insert events
+        if r['eventName']!='INSERT': continue
         entry = r['dynamodb']['NewImage']
-        message += entry['name']['S']+' finished a round with a score of '+str(entry['total']['N'])+'!\n'
-    
-    print(message)
+        message += entry['name']['S']+' finished the game '+entry['title']['S']+' with a score of '+str(entry['total']['N'])+'!\n'
+        if entry['comment']['S']: message+='They left the following comment:\n'+entry['comment']['S']+'\n'
     
     topic_arn = os.environ['SNS_TOPIC_ARN']
     subject = "Project Yacht Was Played"
     sns = boto3.client('sns')
     response = sns.publish(TopicArn=topic_arn,Message=message,Subject=subject)
-    
-    print(response)
     
     return {
         'statusCode': 200,
