49de906e4b9c4cb7661bc42168338cc10f90ae77
Garrett Wilhelm <26906320+gbwilhelm@users.noreply.github.com>
2021-05-24 18:42:15 -0400

    Added leaderboard score card

--- web/public/leaderboard.js
+++ web/public/leaderboard.js
@@ -2,17 +2,8 @@ var app = new Vue({
   el: '#leaderboard',
   data: {
     //user must refresh the page to fetch any updates to leaderboard
-    entries: []
-      //test data to avoid pinging database
-    /*entries: [{name:{S:'testName10'},title:{S:'testTitle10'},total:{N:10},comment:{S:'testComment10'},public:{BOOL:true}},
-              {name:{S:'privateCommentTest'},title:{S:'testTitle7'},total:{N:7},comment:{S:''},public:{BOOL:false}},
-              {name:{S:'webTest5'},title:{S:'testTitle5'},total:{N:5},comment:{S:''},public:{BOOL:false}},
-              {name:{S:'newLambdaTest'},title:{S:'testTitle4'},total:{N:4},comment:{S:''},public:{BOOL:false}},
-              {name:{S:'testName3'},title:{S:'testTitle3'},total:{N:3},comment:{S:''},public:{BOOL:true}},
-              {name:{S:'Max Width Test'},title:{S:'This title has the max charact'},total:{N:1},comment:{S:'This comment has the max limit of 30\
-              0 characters. AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\
-              AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\
-              A'},public:{BOOL:true}}]*/
+    entries: [],
+    currentEntry: -1
   },
   methods: {
     fetchData: async function(data){
@@ -20,6 +11,9 @@ var app = new Vue({
       xmlHttp.open("GET","/ddb",false)
       xmlHttp.send() //blocking send
       return xmlHttp
+    },
+    radioClick: function(index){
+      this.currentEntry = index
     }
   },
   mounted(){
